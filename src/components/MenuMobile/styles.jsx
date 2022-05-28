87fa70d4bc1d46db5e6f6f31c542c3fcd0dcbe91
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-05-28 14:58:52 -0300

    feat: alterando tamanho da fonte

--- src/components/MenuMobile/styles.jsx
+++ src/components/MenuMobile/styles.jsx
@@ -1,33 +1,39 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 export const Container = styled.div`
-position: fixed;
-width: 100%;
-height: 100%;
-top: 0;
-left: 0;
-right: 0;
-bottom: 0;
-z-index: 5;
-display: flex;
-align-items: center;
-justify-content: center;
-backdrop-filter: blur(3px);
-background: rgba(17, 18, 17, 0.95);
-opacity: 0;
-pointer-events: none;
-transition: 0.2s;
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 5;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  backdrop-filter: blur(3px);
+  background: rgba(17, 18, 17, 0.95);
+  opacity: 0;
+  pointer-events: none;
+  transition: 0.2s;
 
-${({ isVisible }) => isVisible && `
+  ${({ isVisible }) =>
+    isVisible &&
+    `
 opacity: 1;
 pointer-events: auto;
 `}
-`;
+`
 
 export const Nav = styled.nav`
-display: flex;
-align-items: center;
-justify-content: center;
-flex-direction: column;
-gap: 2rem;
-`;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 2rem;
+
+  a {
+    font-size: 3rem;
+  }
+`
